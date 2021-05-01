6df6a079452092ce56c0a27ad2aaee586c06014a
Aygyun <49846915+achu94@users.noreply.github.com>
2021-05-01 14:53:30 +0200

    footer angepasst.

--- client/src/Pages/Footer/Footer.js
+++ client/src/Pages/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
 
   return(
       <div className="main footer">
-        <p style={{float: 'right', color: "#FFC85A"}} >&copy; 2021 HM TROCKENBAU</p>
+        <p style={{float: 'right', color: "#E89B00"}} >&copy; 2021 HM TROCKENBAU</p>
       </div>
   )
 };
