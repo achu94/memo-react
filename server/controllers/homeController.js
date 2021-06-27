58bbf1c39500a0c91f9e034665ef4e7b1d1542b1
achu94 <aygyun_94@outlook.de>
2021-06-27 22:48:43 +0200

    Revert "preparing for admin panel"
    This reverts commit afd29fb401474cc8ad76e05af54397204470ba18.

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -4,6 +4,7 @@ const path = require('path');
 const router = Router();
 //if (!process.env.NODE_ENV == 'development'){
     router.get('/', (req, res) => {
+        console.log('AMK')
         res.sendFile(path.resolve('build', 'index.html'));
     });
 //}
